5d669835c9f692d8bd61b1f1e8b6913832a2bb94
RicardoBordon <rikyyy09@gmail.com>
2026-08-03 13:36:47 -0300

    otro mail

--- api/sendMail.js
+++ api/sendMail.js
@@ -108,7 +108,7 @@ export default async function handler(req, res) {
 
     await transporter.sendMail({
       from: gmail.email,
-      to: "rikyyy09@gmail.com",
+      to: ["rikyyy09@gmail.com","tecnicoscaseros@gmail.com"],
       subject: `${sala} | ${categoriaAsunto} | UID ${uid} | ${subcategoria}`,
       text: `
 Categoría: ${categoria} — ${subcategoria}
